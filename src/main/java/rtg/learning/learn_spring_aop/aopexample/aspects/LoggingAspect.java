480c004201d97493d1e77b50281959c8985418a6
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-04 12:28:26 +0530

    Added a Logging Aspect

---
+++ src/main/java/rtg/learning/learn_spring_aop/aopexample/aspects/LoggingAspect.java
@@ -0,0 +1,20 @@
+package rtg.learning.learn_spring_aop.aopexample.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+public class LoggingAspect {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	//Logic of the method
+	@Before("execution(* rtg.learning.learn_spring_aop.aopexample.business.*.*(..))")
+	public void logMethodCall(JoinPoint  joinPoint) {
+		logger.info("Before Aspect - Method is called - {}", joinPoint);
+	}
+}
