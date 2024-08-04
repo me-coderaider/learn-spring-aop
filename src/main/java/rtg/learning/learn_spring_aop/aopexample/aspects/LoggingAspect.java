450fb547bac131706203ce8b4a4db084a9428aba
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-04 16:26:57 +0530

    AOP Annotations @Before, @After, @AfterReturning, @AfterThrowing

--- src/main/java/rtg/learning/learn_spring_aop/aopexample/aspects/LoggingAspect.java
+++ src/main/java/rtg/learning/learn_spring_aop/aopexample/aspects/LoggingAspect.java
@@ -1,6 +1,9 @@
 package rtg.learning.learn_spring_aop.aopexample.aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -13,8 +16,27 @@ public class LoggingAspect {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	
 	//Logic of the method
-	@Before("execution(* rtg.learning.learn_spring_aop.aopexample.business.*.*(..))")
-	public void logMethodCall(JoinPoint  joinPoint) {
-		logger.info("Before Aspect - Method is called - {}", joinPoint);
+	@Before("execution(* rtg.learning.learn_spring_aop.aopexample.*.*.*(..))")
+	public void logMethodCallBeforeExecution(JoinPoint  joinPoint) {
+		logger.info("Before Aspect - {} Method is called, with args - {}", joinPoint, joinPoint.getArgs());
+	}
+	
+	@After("execution(* rtg.learning.learn_spring_aop.aopexample.*.*.*(..))")
+	public void logMethodCallAfterExecution(JoinPoint  joinPoint) {
+		logger.info("After Aspect - {} Method is called", joinPoint);
+	}
+	
+	@AfterThrowing(pointcut="execution(* rtg.learning.learn_spring_aop.aopexample.*.*.*(..))", 
+			throwing = "exception"
+			)
+	public void logMethodCallAfterException(JoinPoint  joinPoint, Exception exception) {
+		logger.info("AfterThrowing Aspect - {} Method has thrown an exception - {}", joinPoint, exception);
+	}
+	
+	@AfterReturning(pointcut="execution(* rtg.learning.learn_spring_aop.aopexample.*.*.*(..))", 
+			returning = "resultValue"
+			)
+	public void logMethodCallAfterSuccessfulExecution(JoinPoint  joinPoint, Object resultValue) {
+		logger.info("AfterReturning Aspect - {} Method has returned - {}", joinPoint, resultValue);
 	}
 }
